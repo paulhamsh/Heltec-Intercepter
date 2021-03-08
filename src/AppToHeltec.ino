f6a9489d405c265383ae61b630fc6b8539541be2
paulhamsh <p.w.hamshere@gmail.com>
2021-03-08 16:19:50 +0000

    Add files via upload

---
+++ src/AppToHeltec.ino
@@ -0,0 +1,74 @@
+#include "BluetoothSerial.h" //Header File for Serial Bluetooth, will be added by default into Arduino
+#include "heltec.h"
+
+BluetoothSerial SerialBT; //Object for Bluetooth
+HardwareSerial HWSerial(2);
+
+#define HW_BAUD 1000000
+#define PGM_NAME "App - Heltec"
+
+uint8_t bt_byte, ser_byte;
+uint8_t buf[5000];
+int pos, len;
+int scr_line;
+
+void printit(char *str) {
+  if (scr_line >= 8) {
+    Heltec.display->clear();
+    Heltec.display->drawString(0, 0, PGM_NAME);
+    scr_line = 1;
+  }
+  Heltec.display->drawString(0,scr_line *8, str);
+  Heltec.display->display();
+  scr_line++;
+}
+
+#define SPARK_BT_NAME "Spark 40 Audio"
+
+void setup() {
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/);
+  HWSerial.begin(HW_BAUD, SERIAL_8N1, 14, 12);
+
+  Heltec.display->clear();
+  Heltec.display->drawString(0, 0, PGM_NAME);
+  Heltec.display->display();
+  scr_line = 1;
+  
+  printit("Starting..");
+  if (!SerialBT.begin(SPARK_BT_NAME)) 
+  {
+    printit("Failed"); 
+    while (true) {};
+  }
+  printit("Ready ");
+
+  pos = 0;
+  len = -1;
+}
+
+char str[40];
+
+void loop() {
+  
+  while (SerialBT.available()) {
+    bt_byte = SerialBT.read();  
+    HWSerial.write(bt_byte);    
+  };
+  
+  while (HWSerial.available()) {
+    ser_byte = HWSerial.read();
+    
+    buf[pos++] = ser_byte;
+    if (pos == 7) {
+      len = ser_byte; 
+    }
+    if (pos == len) {
+      SerialBT.write(buf, pos);    
+      pos = 0;
+      len = -1;  
+      
+      snprintf(str, sizeof(str), "> %2.2x %2.2x %2.2x", buf[6], buf[20], buf[21]);
+      printit(str);
+    }
+  }
+}
